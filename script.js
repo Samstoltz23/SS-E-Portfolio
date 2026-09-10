f8db3b8978300e47aebf2f9c67633d8d8f4bf1ba
Samantha Stoltzfus <samantha.stoltzfus23@gmail.com>
2026-09-10 13:43:01 -0400

    javascript modal

--- script.js
+++ script.js
@@ -4,14 +4,29 @@
 
 function contact(event) {
   event.preventDefault();
+  const loading = document.querySelector('modal__overlay--loading');
+  const success = document.querySelector('modal__overlay--success');
+  loading.classList += " modal__overlay--visible"
   emailjs
     .sendForm(
-      "service_qdhngdm",
-      "template_hecmjif",
+      'service_qdhngdm',
+      'template_hecmjif',
       event.target,
-      "-10dBvubUG7zjCqyA",
-    )
-    .then(() => {
-      console.log("this worked");
-    });
+      '-10dBvubUG7zjCqyA'
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable. Please contact me directly at samantha.stoltzfus23@gmail.com"
+    );
+  })
 }
+
+
+function toggleModal(){
+  document.body.classList.toggle("modal--open");
+}
+
+
