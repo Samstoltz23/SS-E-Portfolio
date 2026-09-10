33f799e8ea2c696cf51c4c0d18ab55772d7abaca
Samantha Stoltzfus <samantha.stoltzfus23@gmail.com>
2026-09-09 21:29:17 -0400

    nav and header overlap

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function toggleModal(){
-  document.body.classList.toggle("modal--open");
-}
+//template_hecmjif
+//service_qdhngdm
+//-10dBvubUG7zjCqyA
 
+function contact(event) {
+  event.preventDefault();
+  emailjs
+    .sendForm(
+      "template_hecmjif",
+      "service_qdhngdm",
+      event.target,
+      "-10dBvubUG7zjCqyA",
+    )
+    .then(() => {
+      console.log("this worked");
+    });
+}
